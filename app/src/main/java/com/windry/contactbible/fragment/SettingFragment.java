26f366ab9ee1d4e15c27b5820b6e744a9daefb43
SeungGun <saverrw2864@naver.com>
2021-06-19 18:11:34 +0900

    ready to update for 1.1.6 version

--- app/src/main/java/com/windry/contactbible/fragment/SettingFragment.java
+++ app/src/main/java/com/windry/contactbible/fragment/SettingFragment.java
@@ -1,4 +1,4 @@
-package com.windry.contactbible;
+package com.windry.contactbible.fragment;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.widget.Toast;
 
+import com.windry.contactbible.R;
 import com.windry.contactbible.database.ReadDBHelper;
 
 import androidx.annotation.Nullable;
